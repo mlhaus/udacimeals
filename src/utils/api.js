1273a8da919d17f70d3a06f7e44d9aed37003126
Marc Hauschildt <marc.hauschildt@gmail.com>
2017-09-24 23:33:47 -0500

    Setup Calendar Grid, Food Modal, and edamam api

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,8 @@ const APP_KEY = process.env.REACT_APP_APP_KEY
 export function fetchRecipes (food = '') {
 	food = food.trim()
 
-	return fetch(`https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+	//return fetch(`https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+	return fetch(`https://api.edamam.com/search?q=${food}&app_id=01388bd6&app_key=e539e9d37622303d4d9d35e726021644`)
 		.then((res) => res.json())
 		.then(({ hits }) => hits.map(({ recipe }) => recipe))
 }
